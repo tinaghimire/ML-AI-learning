e31fbf7a9d2f408c127f2455d2b4d96ea5ad29db
tinaghimire <ghimirekristina10@gmail.com>
2023-10-17 13:51:17 +0545

    Completed data types and operators in python

--- PythonRule.py
+++ PythonRule.py
@@ -27,3 +27,15 @@ print("Does your {} {}?".format(animal, action))
 
 maria_string = "Maria loves {} and {}" 
 print(maria_string.format("math", "statistics"))
+
+# String method: split()
+new_str = "The cow jumped over the moon."
+print(new_str.split())
+print(new_str.split(' ', 3)) # Here the separator is space, and the maxsplit argument is set to 3.
+print(new_str.split('.'))    # Period as a separator.
+print(new_str.split(None, 4)) # Using no separators but having a maxsplit argument of 3.
+
+str_count = "One fish, two fish, red fish, blue Fish"
+# To find the first occurence of word
+print(str_count.index('fish'))
+print(str_count.rindex('fish'))
