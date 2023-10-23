963aafdb9b1df5ba60fa781ac068e730b64b5bc0
tinaghimire <ghimirekristina10@gmail.com>
2023-10-23 17:43:17 +0545

    List methods added

--- DataStructure.py
+++ DataStructure.py
@@ -11,3 +11,10 @@ half_mon = months[:4] # 'Jan', 'Feb', 'Mar', 'Apr'
 # Use of in and not in list
 print('Apr' in months)
 print('Oct' not in months)
+
+# Methods
+num = [21, 2, 15, 98, 12]
+print(len(num))
+print(max(num))
+print(min(num))
+print(sorted(num))
