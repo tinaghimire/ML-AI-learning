8036205c2195914785f192b27dcde59c562a5ec6
tinaghimire <ghimirekristina10@gmail.com>
2023-10-25 23:13:40 +0545

    Data structure completed

--- DataStructure.py
+++ DataStructure.py
@@ -33,3 +33,64 @@ num_word.append('Fifty')
 num.append(50)
 print(num)
 print(num_word)
+
+# Tuple
+location = (13.4125, 103.866667)
+print("Latitude:", location[0])
+print("Longitude:", location[1])
+
+# Tuple with multiple variable names
+dimensions = 52, 40, 100
+length, width, height = dimensions  # Called Tuple unpacking
+print("The dimensions are {} x {} x {}".format(length, width, height))
+
+# Sets
+numbers = [1, 2, 6, 3, 1, 1, 6]
+unique_num = set(numbers)    #  {1, 2, 6, 3}
+print(unique_num)
+
+# Methods in set
+fruit = {"apple", "banana", "orange", "grapefruit"}
+print("watermelon" in fruit)
+
+fruit.add("watermelon")
+print(fruit)
+
+print(fruit.pop())
+print(fruit)
+
+# Dictionary in python
+elements = {"hydrogen": 1, "helium": 2, "carbon": 6}
+print(elements["helium"])   # 2
+elements["lithium"] = 3     # Values can be changed
+print(elements)
+
+# in keyword
+print("carbon" in elements)
+print(elements.get("dilithium"))
+
+# Identity operators: is and is not
+n = elements.get("dilithium")
+print(n is None)
+print(n is not None)
+
+# get method with default value "No such element"
+print(elements.get("dilithium", "No such element"))
+
+# Compound data structures
+elements = {"hydrogen": {"number": 1,
+                         "weight": 1.00794,
+                         "symbol": "H"},
+              "helium": {"number": 2,
+                         "weight": 4.002602,
+                         "symbol": "He"}}
+
+helium = elements["helium"]  # get the helium dictionary
+hydrogen_weight = elements["hydrogen"]["weight"]  # get hydrogen's weight
+oxygen = {"number":8,"weight":15.999,"symbol":"O"}  # create a new oxygen dictionary 
+elements["oxygen"] = oxygen  # assign 'oxygen' as a key to the elements dictionary
+print('elements = ', elements)
+
+elements['hydrogen']['is_noble_gas'] = False
+elements['helium']['is_noble_gas'] = True
+elements['oxygen']['is_noble_gas'] = False
